1d32e089c56984ad2c294d94b453ac74c2a299d9
Varun Malaviya <varunmalaviya7@gmail.com>
2023-01-09 20:19:19 -0500

    added a new component to display a given grid of photos.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import NavBar from './Components/NavBar';
+import MainPhotoGrid from './Components/MainPhotoGrid';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <MainPhotoGrid />
     </div>
   );
 }
---
+++ src/Components/MainPhotoGrid.js
@@ -0,0 +1,9 @@
+function MainPhotoGrid(){
+    return (
+        <div className="MainPhotoGrid">
+            <img src="main-photos-grid.png" alt="a grid of photos showcasing people doing activities that are available at AirBnB" />
+        </div>
+    )
+}
+
+export default MainPhotoGrid
