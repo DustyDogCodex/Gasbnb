5186d693522b3359f03151e7e48d9b60baa270db
Varun Malaviya <varunmalaviya7@gmail.com>
2023-01-09 20:19:19 -0500

    added an intro component that provides an intro to the website.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from './Components/NavBar';
 import MainPhotoGrid from './Components/MainPhotoGrid';
+import Intro from './Components/Intro';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <MainPhotoGrid />
+      <Intro />
     </div>
   );
 }
