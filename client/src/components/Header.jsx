0ede796d8ef6ac9d5f929c8cee0ca78e200faf74
Varun Malaviya <varunmalaviya7@gmail.com>
2023-05-31 23:24:59 -0400

    added more elements to the header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faAirbnb } from "@fortawesome/free-brands-svg-icons" 
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import { faMagnifyingGlass, faBars, faCircleUser, faGlobe } from "@fortawesome/free-solid-svg-icons"
 
 function Header(){
     return(
-        <header className="flex items-center">
+        <header className="flex items-center justify-between">
             <div className="flex items-center p-5">
                 <FontAwesomeIcon 
                     icon={faAirbnb} 
@@ -20,8 +20,26 @@ function Header(){
                 <button className="bg-red-600/80 h-10 w-10 rounded-full flex items-center justify-center ml-3 py-1 px-4">
                     <FontAwesomeIcon 
                         icon={faMagnifyingGlass} 
-                        style={{color: "#fffafa",}} />
+                        style={{color: "#fffafa",}} 
+                    />
                 </button>
+            </div>
+            <div className="flex items-center p-5">
+                <span className="mr-6">Airbnb your home</span>
+                <FontAwesomeIcon 
+                    icon={faGlobe} 
+                    style={{color: "#adadad", height:'20px', marginRight:'20px'}} 
+                />
+                <div className="flex items-center border-2 p-2 rounded-full">
+                    <FontAwesomeIcon 
+                        icon={faBars} 
+                        style={{color: "#7e8186", height:'20px', marginLeft:'8px'}} 
+                    />
+                    <FontAwesomeIcon 
+                        icon={faCircleUser} 
+                        style={{color: "#9e9e9e", height:'30px', marginLeft:'20px'}} 
+                    />
+                </div>
             </div>
         </header>
     )
