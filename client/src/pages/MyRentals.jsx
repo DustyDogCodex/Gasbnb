51468e8e9fb565fee4d875b35cd391f505e053be
vmalaviya <varunmalaviya7@gmail.com>
2023-06-13 10:55:35 -0400

    added content to myrentals page

--- client/src/pages/MyRentals.jsx
+++ client/src/pages/MyRentals.jsx
@@ -1,7 +1,19 @@
+import { Link } from 'react-router-dom'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function MyRentals() {
   return (
     <div>
-        MyRentals
+        My rentals
+        <div>
+            <Link 
+                to={'/account/rentals/new'}
+                className='bg-red text-white px-4 py-2 rounded-full text-xl'
+            >
+               <FontAwesomeIcon icon={faPlus} /> Add new rental
+            </Link>
+        </div>
     </div>
   )
 }
