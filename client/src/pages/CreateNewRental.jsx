f0f11a9b56f3d504414cc6f34855b7c471e467e3
vmalaviya <varunmalaviya7@gmail.com>
2023-08-11 14:37:41 -0400

    cleaned up form code + added button to cancel the form. Working on improving styling next.

--- client/src/pages/CreateNewRental.jsx
+++ client/src/pages/CreateNewRental.jsx
@@ -87,204 +87,227 @@ function CreateNewRental() {
     }
 
     return (
-    <div>
-        <form 
-            className="w-4/5"
-            onSubmit={handleSubmit(submitData)}
-        >   
+        <div
+            className="flex flex-col items-center border p-3 rounded-lg"
+        >
             <h1>Please add information for your new listing:</h1>
-            <label>Title</label>
-            <input 
-                {...register("title", { required: true })}
-                className="w-full border-2 p-2 rounded-lg"
-                type="text"
-                placeholder="My lovely apartment/house etc" 
-            />
-            <label>Price per night</label>
-            <input 
-                {...register("price", { required: true })}
-                className="w-full border-2 p-2 rounded-lg"
-                type="number"
-                placeholder="$100 per night" 
-            />
-            <label>Location</label>
-            <input 
-                {...register("location", { required: true })}
-                className="w-full border-2 p-2 rounded-lg"
-                type="text"
-                placeholder="Enter a location" 
-            />
-            <label>Description</label>
-            <textarea
-                {...register("description", { required: true, maxLength: 1000 })}
-                className="w-full border-2 p-2 rounded-lg"
-                type="text"
-                placeholder="A brief description of your rental" 
-            />
-            <label>Additional Information</label>
-            <textarea
-                {...register("extraInfo")}
-                className="w-full border-2 p-2 rounded-lg"
-                type="text"
-                placeholder="Any additional information for guests. Ex:house rules, check-in procedure etc" 
-            />
-            <label>Amenities</label>
-            <div
-                className="flex flex-col checkbox"
-            >
-                <label 
-                    className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
-                >
-                    <input 
-                        {...register("checkbox")}
-                        type="checkbox"
-                        className="m-2"  
-                        value="WiFi"  
-                    />
-                    <FontAwesomeIcon icon={faWifi} /> WiFi
-                </label>
 
-                <label
-                    className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
-                >
-                    <input 
-                        {...register("checkbox")}
-                        type="checkbox"
-                        className="m-2"
-                        value="TV"    
-                    />
-                    <FontAwesomeIcon icon={faTv} /> TV
-                </label>
-
-                <label
-                    className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
-                >
-                    <input 
-                        {...register("checkbox")}
-                        type="checkbox"
-                        className="m-2"  
-                        value="Pets Allowed"   
-                    />
-                    <FontAwesomeIcon icon={faPaw} /> Pets Allowed
-                </label>
-
-                <label
-                    className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
-                >
-                    <input 
-                        {...register("checkbox")}
-                        type="checkbox"
-                        className="m-2" 
-                        value="Private Entrance"    
-                    />
-                    <FontAwesomeIcon icon={faDoorClosed} /> Private Entrance
-                </label>
-
-                <label
-                    className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
-                >
-                    <input
-                        {...register("checkbox")} 
-                        type="checkbox"
-                        className="m-2" 
-                        value="Free Parking Spot"    
-                    />
-                    <FontAwesomeIcon icon={faCarSide} /> Free Parking Spot
-                </label>
-
-            </div>
-            <label>Check In</label>
-            <input 
-                {...register("checkIn")} 
-                className="w-full border-2 p-2 rounded-lg"
-                type="time"
-                placeholder="Check in times" 
-            />
-            <label>Check Out</label>
-            <input 
-                {...register("checkOut")}
-                className="w-full border-2 p-2 rounded-lg"
-                type="time"
-                placeholder="Check out times" 
-            />
-            <label>Maximum number of guests allowed</label>
-            <input 
-                {...register("maxGuests")}
-                className="w-full border-2 p-2 rounded-lg"
-                type="number"
-                placeholder="How many people are allowed to be here at one time" 
-            />
-            <label>Photos</label>
-            <p
-                className="text-sm text-stone-400 my-1"
-            >
-                You can upload photos by using a link to a image hosting website or directly from your device!
-            </p>
-            <div
-                className="flex justify-between"
-            >
+            {/* form for creating a new listing to rent out */}            
+            <form 
+                className="w-4/5"
+                onSubmit={handleSubmit(submitData)}
+            >   
+                <label>Title</label>
                 <input 
-                    name="uploadimages-link"
-                    className="w-4/5 border-2 p-2 rounded-lg"
+                    {...register("title", { required: true })}
+                    className="w-full border-2 p-2 rounded-lg"
                     type="text"
-                    value={imageLink}
-                    onChange={e => setImageLink(e.target.value)}
-                    placeholder="Upload photos with a link" 
+                    placeholder="My lovely apartment/house etc" 
                 />
-                <button
-                    type="button"
-                    className="bg-red text-white text-xl py-2 px-4 rounded-full my-2"
-                    onClick={addLinkImages}
-                >
-                    Add photo
-                </button>
-            </div>
+                
+                <label>Price per night</label>
+                <input 
+                    {...register("price", { required: true })}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="number"
+                    placeholder="$100 per night" 
+                />
             
-            {/* gallery for displaying images queued up to be added to the listing by user */}
-            <div
-                className="mt-3 flex flex-wrap border-2 p-3 rounded-md"
-            >
-                {/* calling the saved files from our server if they have been uploaded already. if no images have been placed in the queue, a simple "add photos" will be displayed */}
-                {/* server has a static folder with all uploaded images available at route /uploads/file-name.jpg */}
-                {imageQueue.length > 0 && imageQueue.map(photo => 
-                    <img
-                        key={photo}
-                        className="max-w-[300px] max-h-[300px] rounded-xl m-1"
-                        src={`http://localhost:5000/uploads/${photo}`}
-                        alt="uploaded image"
-                    />
-                )}
+                <label>Location</label>
+                <input 
+                    {...register("location", { required: true })}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="text"
+                    placeholder="Enter a location" 
+                />
+            
+                <label>Description</label>
+                <textarea
+                    {...register("description", { required: true, maxLength: 1000 })}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="text"
+                    placeholder="A brief description of your rental" 
+                />
+            
+                <label>Additional Information</label>
+                <textarea
+                    {...register("extraInfo")}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="text"
+                    placeholder="Any additional information for guests. Ex:house rules, check-in procedure etc" 
+                />
 
-                <label
-                    className="border-2 rounded-xl p-2 flex items-center gap-2 font-roboto font-semibold cursor-pointer text-stone-500 m-1"
+                {/* amenities checkboxes */}
+                <label>Amenities</label>
+                <div
+                    className="flex flex-col checkbox"
                 >
-                    <FontAwesomeIcon 
-                        icon={faCloudArrowUp} 
-                        style={{height:'40px', width:'40px', color:'grey'}}
-                    />
-                    Upload images
-                    <input 
-                        multiple
-                        className="hidden"
-                        type="file" 
-                        onChange={addDeviceImages}
-                    />
-                </label>
-            </div>
-            <div
-                className="flex items-center justify-center mt-8"
-            >
-                <button 
-                    type="submit"
-                    className="bg-red text-white text-2xl py-2 px-4 rounded-full self-center"    
-                >
-                    Save
-                </button>
-            </div>
+                    <label 
+                        className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
+                    >
+                        <input 
+                            {...register("checkbox")}
+                            type="checkbox"
+                            className="m-2"  
+                            value="WiFi"  
+                        />
+                            <FontAwesomeIcon icon={faWifi} /> WiFi
+                        </label>
+
+                    <label
+                        className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
+                    >
+                        <input 
+                            {...register("checkbox")}
+                            type="checkbox"
+                            className="m-2"
+                            value="TV"    
+                        />
+                        <FontAwesomeIcon icon={faTv} /> TV
+                    </label>
+
+                    <label
+                        className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
+                    >
+                        <input 
+                            {...register("checkbox")}
+                            type="checkbox"
+                            className="m-2"  
+                            value="Pets Allowed"   
+                        />
+                        <FontAwesomeIcon icon={faPaw} /> Pets Allowed
+                    </label>
+
+                    <label
+                        className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
+                    >
+                        <input 
+                            {...register("checkbox")}
+                            type="checkbox"
+                            className="m-2" 
+                            value="Private Entrance"    
+                        />
+                        <FontAwesomeIcon icon={faDoorClosed} /> Private Entrance
+                    </label>
+
+                    <label
+                        className="border-2 py-2 pr-10 w-1/3 rounded-md m-2 cursor-pointer"
+                    >
+                        <input
+                            {...register("checkbox")} 
+                            type="checkbox"
+                            className="m-2" 
+                            value="Free Parking Spot"    
+                        />
+                        <FontAwesomeIcon icon={faCarSide} /> Free Parking Spot
+                    </label>
+                </div>
+
+                <label>Check In</label>
+                <input 
+                    {...register("checkIn")} 
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="time"
+                    placeholder="Check in times" 
+                />
             
-        </form>
-    </div>
-  )
+                <label>Check Out</label>
+                <input 
+                    {...register("checkOut")}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="time"
+                    placeholder="Check out times" 
+                />
+            
+                <label>Maximum number of guests allowed</label>
+                <input 
+                    {...register("maxGuests")}
+                    className="w-full border-2 p-2 rounded-lg"
+                    type="number"
+                    placeholder="How many people are allowed to be here at one time" 
+                />
+            
+                <label>Photos</label>
+                <p
+                    className="text-sm text-stone-400 my-1"
+                >
+                    You can upload photos by using a link to a image hosting website or directly from your device!
+                </p>
+                
+                {/* add photos section */}
+                <div
+                    className="flex justify-between"
+                >
+                    <input 
+                        name="uploadimages-link"
+                        className="w-4/5 border-2 p-2 rounded-lg"
+                        type="text"
+                        value={imageLink}
+                        onChange={e => setImageLink(e.target.value)}
+                        placeholder="Upload photos with a link" 
+                    />
+                    <button
+                        type="button"
+                        className="bg-red text-white text-xl py-2 px-4 rounded-full my-2"
+                        onClick={addLinkImages}
+                    >
+                        Add photo
+                    </button>
+                </div>
+            
+                {/* gallery for displaying images queued up to be added to the listing by user */}
+                <div
+                    className="mt-3 flex flex-wrap border-2 p-3 rounded-md"
+                >
+                    {/* calling the saved files from our server if they have been uploaded already. if no images have been placed in the queue, a simple "add photos" will be displayed */}
+                    {/* server has a static folder with all uploaded images available at route /uploads/file-name.jpg */}
+                    {imageQueue.length > 0 && imageQueue.map(photo => 
+                        <img
+                            key={photo}
+                            className="max-w-[300px] max-h-[300px] rounded-xl m-1"
+                            src={`http://localhost:5000/uploads/${photo}`}
+                            alt="uploaded image"
+                        />
+                    )}
+
+                    <label
+                        className="border-2 rounded-xl p-2 flex items-center gap-2 font-roboto font-semibold cursor-pointer text-stone-500 m-1"
+                    >
+                        <FontAwesomeIcon 
+                            icon={faCloudArrowUp} 
+                            style={{height:'40px', width:'40px', color:'grey'}}
+                        />
+                        Upload images
+                        <input 
+                            multiple
+                            className="hidden"
+                            type="file" 
+                            onChange={addDeviceImages}
+                        />
+                    </label>
+                </div>
+                
+                {/* Save or cancel the form */}
+                <div
+                    className="flex items-center justify-center mt-8"
+                >
+                    <button 
+                        type="submit"
+                        className="bg-red text-white text-2xl py-2 px-4 rounded-full self-center"    
+                    >
+                        Save
+                    </button>
+
+                    <button 
+                        type="button"
+                        className="bg-sky-300 text-white text-2xl py-2 px-4 rounded-full self-center"    
+                    >
+                        Cancel
+                    </button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default CreateNewRental
