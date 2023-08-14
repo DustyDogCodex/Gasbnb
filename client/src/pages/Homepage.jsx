c99db82a6f903d1949fced7719c0538d97159d1d
vmalaviya <varunmalaviya7@gmail.com>
2023-08-14 12:10:50 -0400

    added pikachu loading animation to homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -2,16 +2,23 @@ import axios from "axios"
 import { NavBar } from "../components/NavBar"
 import { useEffect, useState } from 'react'
 import ListingDisplayBox from "../components/ListingDisplayBox"
+import pikachu from '../assets/pikachu.gif'
 
 function Homepage() {
     //state variable to store all available listings
     const [ listings, setListings ] = useState([])
 
+    //toggle for loading animation while data is fetched from server
+    const [ loading, setLoading ] = useState(true)
+
     //using useEffect to make an API call to retrieve all available listings and display it on homepage
     useEffect(() => {
         const getListings = async() =>{
             axios.get("http://localhost:5000/listings/available")
-            .then(res => setListings(res.data))
+            .then(res => {
+                setListings(res.data)
+                setLoading(false)
+            })
             .catch(err => console.log(err))
         }
         getListings()
@@ -24,16 +31,32 @@ function Homepage() {
             <div
                 className="flex items-center justify-center"
             >
-                <div
-                    className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 p-8 w-screen xl:w-[90%]"
-                >
-                    {listings.length > 0 && listings.map((listing,index) => 
-                        <ListingDisplayBox 
-                            key={index} 
-                            listing={listing}
-                        /> 
-                    )}
-                </div>
+                {loading 
+                    ?
+                    /* loading screen with a running pikachu animation :) */
+                    (
+                        <div 
+                            className="w-full h-screen flex justify-center items-center"
+                        >
+                            <img 
+                                src={pikachu}
+                                alt="pikachu running loading animation" 
+                                className="w-60 h-60"
+                            />
+                        </div>
+                    )
+                    :
+                    <div
+                        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-5 p-8 w-screen xl:w-[90%]"
+                    >
+                        {listings.length > 0 && listings.map((listing,index) => 
+                            <ListingDisplayBox 
+                                key={index} 
+                                listing={listing}
+                            /> 
+                        )}
+                    </div>
+                }
             </div>
         </>
     )
