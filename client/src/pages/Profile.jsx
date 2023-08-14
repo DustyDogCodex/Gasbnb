29bfa74f2dd13b9097288fe0cd4ff7523b732c1f
vmalaviya <varunmalaviya7@gmail.com>
2023-08-14 17:44:09 -0400

    Chnaged profile subpage in account page. Added ability to change settings by toggling edit button on each individual setting. Setting up server routes for operations next.

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,79 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { UserContext } from "../UserContext"
-import AccountInfo from "../components/AccountInfo"
 import axios from "axios"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faPenToSquare, faCheck, faXmark } from "@fortawesome/free-solid-svg-icons"
 
 function Profile() {
      //getting userInfo from context
     const { userInfo } = useContext(UserContext)
+
+    //variables to track user input
+    const [ name, setName ] = useState('')
+    const [ email, setEmail ] = useState('')
+    const [ profilePic, setProfilePic ] = useState('')
+
+    //toggle inputs to enter new user info
+    const [ editName, setEditName ] = useState(false)
+    const [ editEmail, setEditEmail ] = useState(false)
+    const [ editPicture, setEditPicture ] = useState(false)
+
+    //api calls for updating user settings
+    async function updateName(){
+        axios.put(`http://localhost:5000/settings/name`,
+            { userId: userInfo._id, name },
+            { withCredentials: true }
+        )
+        .then(res => 
+            {
+                if (res) { 
+                    window.location.reload()
+                }
+            }
+        )
+        .catch(err => console.log(err))
+    }
+
+    async function updateEmail(){
+        axios.put(`http://localhost:5000/settings/email`,
+            { userId: userInfo._id, email },
+            { withCredentials: true }
+        )
+        .then(res => 
+            {
+                if (res) { 
+                    window.location.reload()
+                }
+            }
+        )
+        .catch(err => console.log(err))
+    }
+
+    async function updateProfilePic(){
+        //submitting formData with uploaded image to update post
+        const formData = new FormData()
+        
+        formData.append('userId', userInfo._id)
+        
+        if(profilePic){
+            formData.append("image", profilePic)
+        }
+
+        //sending patch request to update post info on server
+        axios.put(`http://localhost:5000/settings/profilepic`,
+            formData,
+            {
+                headers: { "Content-Type": "multipart/form-data" }
+            },
+            { withCredentials: true } 
+        )
+        .then(res => {
+            if(res){
+                window.location.reload()
+            }
+        })
+        .catch(err => console.log(err))
+    }
 
     //function to logout user
     async function logout(){
@@ -42,20 +110,131 @@ function Profile() {
                 </span>
             </p> 
 
-            {/* user account info */}
             <div
-                className="flex flex-col items-center justify-start p-5"
+                className="mt-5 flex items-center justify-between w-3/5 pb-4 border-b border-fuchsia-500"
             >
-                <h2 
-                    className="text-2xl border-b-2 border-fuchsia-300"
+                <label className="font-bold text-xl">Name</label>
+                <div
+                    className={`${editName ? 'hidden' : ''} flex items-center justify-around`}
                 >
-                    Personal Info
-                </h2>
-                
-                <AccountInfo info={"Legal Name"} value={userInfo.name} />
-                <AccountInfo info={"Email address"} value={userInfo.email} />
-                <AccountInfo info={"Profile Picture"} value={userInfo.avatar} />
+                        <p 
+                            className="ml-2 dark:text-white"
+                        >
+                            {userInfo.name}
+                        </p>
+                        <FontAwesomeIcon 
+                            icon={faPenToSquare} 
+                            style={{color: "indigo", cursor:'pointer', marginLeft:'10px'}} 
+                            onClick={() => setEditName(!editName)}
+                        />
+                </div>
+
+                {/* this section will be displayed after user clicks the edit button first */}
+                <div
+                    className={`${editName ? '' : 'hidden'}`}
+                >
+                    <input 
+                        type="text"
+                        value={name} 
+                        onChange={(e) => setName(e.target.value)}
+                        className="rounded-lg p-1 border border-sky-300 ml-3"
+                    />
+                    <FontAwesomeIcon 
+                        icon={faCheck} 
+                        style={{color: "#05fa2e", cursor:'pointer', marginLeft:'5px'}} 
+                        onClick={updateName}
+                    />
+                    <FontAwesomeIcon 
+                        icon={faXmark} 
+                        style={{color: "#ff0000", cursor:'pointer', marginLeft:'5px'}}
+                        onClick={() => setEditName(!editName)} 
+                    />
+                </div>
             </div>
+
+            <div
+                className="mt-5 flex items-center justify-between w-3/5 pb-4 border-b border-fuchsia-500"
+            >
+                <label className="font-bold text-xl">Email</label>
+                <div
+                    className={`${editEmail ? 'hidden' : ''} flex items-center justify-around`}
+                >
+                    <p 
+                        className="ml-5 dark:text-white"
+                    >
+                        {userInfo.email}
+                    </p>
+                    <FontAwesomeIcon 
+                        icon={faPenToSquare} 
+                        style={{color: "indigo", cursor:'pointer', marginLeft:'10px'}} 
+                        onClick={() => setEditEmail(!editEmail)}
+                    />
+                </div>
+
+                {/* this section will be displayed after user clicks the edit button first */}
+                <div
+                    className={`${editEmail ? '' : 'hidden'}`}
+                >
+                    <input 
+                        type="text"
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value)}
+                        className="rounded-lg p-1 border border-sky-300"
+                    />
+                    <FontAwesomeIcon 
+                        icon={faCheck} 
+                        style={{color: "#05fa2e", cursor:'pointer', marginLeft:'5px'}}
+                        onClick={updateEmail} 
+                    />
+                    <FontAwesomeIcon 
+                        icon={faXmark} 
+                        style={{color: "#ff0000", cursor:'pointer', marginLeft:'5px'}}
+                        onClick={() => setEditEmail(!editEmail)} 
+                    />
+                </div>
+            </div>
+
+            <div
+                className="mt-5 flex items-center justify-between w-3/5 pb-4 border-b border-fuchsia-500"
+            >
+                        <label className="font-bold text-xl">Profile Picture</label>
+                        
+                        <div
+                            className={`${editPicture ? 'hidden' : ''} flex items-center justify-around`}
+                        >
+                            <img 
+                                src={`http://localhost:5000/uploads/${userInfo.avatar}`} 
+                                alt="user profile picture" 
+                                className="w-48 h-48 rounded-lg ml-10"
+                            />
+                            <FontAwesomeIcon 
+                                icon={faPenToSquare} 
+                                style={{color: "indigo", cursor:'pointer', marginLeft:'10px'}}
+                                onClick={() => setEditPicture(!editPicture)}
+                            />
+                        </div>
+
+                        {/* this section will be displayed after user clicks the edit button first */}
+                        <div
+                            className={`${editPicture ? '' : 'hidden'}`}
+                        >
+                            <input 
+                                type="file"
+                                onChange={(e) => setProfilePic(e.target.files[0])}
+                                className="rounded-lg p-1 border border-sky-500 w-4/5"
+                            />
+                            <FontAwesomeIcon 
+                                icon={faCheck} 
+                                style={{color: "#05fa2e", cursor:'pointer', marginLeft:'5px'}} 
+                                onClick={updateProfilePic}
+                            />
+                            <FontAwesomeIcon 
+                                icon={faXmark} 
+                                style={{color: "#ff0000", cursor:'pointer', marginLeft:'5px'}}
+                                onClick={() => setEditPicture(!editPicture)} 
+                            />
+                        </div>
+                    </div>
 
             {/* logout button */}
             <button
