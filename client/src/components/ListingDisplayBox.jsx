6d13fb65c1ca2b0b05ba22ce872ada7613e02d8c
vmalaviya <varunmalaviya7@gmail.com>
2023-08-24 13:07:05 -0400

    fixing more bugs

--- client/src/components/ListingDisplayBox.jsx
+++ client/src/components/ListingDisplayBox.jsx
@@ -2,36 +2,33 @@ import { Link } from "react-router-dom"
 
 /* this is for displaying all available listings on the homepage. For listings displayed under my Rentals in the users account page, see UserListingDisplayBox */
 function ListingDisplayBox({ listing }) {
-    //extracting all needed info from listing object
-    const { _id, title, location, price, photos } = listing
-
     return (
         <Link
-            to={`listing-page/${_id}`}
+            to={`listing-page/${listing?._id}`}
             className="h-80 xl:h-96 m-3"
         >
             <img 
                 className="w-full h-4/5 rounded-lg" 
-                src={`https://gasbnb-production.up.railway.app/uploads/${photos[0]}`} 
+                src={`https://gasbnb-production.up.railway.app/uploads/${listing?.photos[0]}`} 
                 alt="listing cover image" 
             />
 
             <p
                 className="mt-2 font-semibold text-sm text-wrap"
             >
-                {title}
+                {listing?.title}
             </p>
             
             <p
                 className="text-sm"
             >
-                {location}
+                {listing?.location}
             </p>
 
             <p
                 className="text-sm"
             >
-                <span className="font-bold text-base">${price}</span> night
+                <span className="font-bold text-base">${listing?.price}</span> night
             </p>
         </Link>
     )
